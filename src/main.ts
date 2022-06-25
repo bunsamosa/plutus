cbc6e89f54352f47d7a9793b2f5cfe385965d09e
madhukar01 <mholla8@gmail.com>
2022-06-25 15:07:31 +0530

    feat(init): vue2 app with primevue

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,18 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+import PrimeVue from 'primevue/config';
 
-import App from './App.vue'
-import router from './router'
 
+import App from './App.vue';
+import router from './router';
+
+// create vue app
 const app = createApp(App)
 
-app.use(createPinia())
-app.use(router)
+// add modules and router
+app.use(createPinia());
+app.use(PrimeVue);
+app.use(router);
 
-app.mount('#app')
+// mount app
+app.mount('#app');
